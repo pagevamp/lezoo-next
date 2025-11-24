0fb949878b53e7fb344b79b2894f7f3e913cabd6
priosshrsth <shrsthprios@hotmail.com>
2025-11-24 11:18:15 +0000

    remove --webpack

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,15 @@ export default function Home() {
       <Leadspace />
       <section className="fixed  top-[110px] md:top-[61px] right-[-10px] z-50 flex flex-col gap-10 justify-center items-center">
         <Button variant="vertical" buttonText={"Playtest"} />
-        <a href="#" className="text-white hover:opacity-80 transition-opacity" aria-label="Instagram">
+        <a
+          href="#"
+          className="text-white hover:opacity-80 transition-opacity"
+          aria-label="Instagram"
+        >
           <DiscordBlueBg className="w-[45px] h-[45px]" />
         </a>
       </section>
-      <Banner/>
+      <Banner />
       <RewardsCarousel />
       <ThreeCards />
       <FiftyFiftySection />
